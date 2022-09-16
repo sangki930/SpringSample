ade1af07e52bee188265bcc682e3776aa5aa35e4
sangkipark <sangki931@gmail.com>
2022-09-16 23:54:23 +0900

    commit SampleTwo commit

--- SpringDataJpaDemo/src/main/java/com/example/demo/sampletwo/SampleTwo.java
+++ SpringDataJpaDemo/src/main/java/com/example/demo/sampletwo/SampleTwo.java
@@ -2,6 +2,8 @@ package com.example.demo.sampletwo;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -34,4 +36,11 @@ public class SampleTwo extends CommonEntity{
 	private String attr1;
 	
 	private String attr2;
+	
+	@Enumerated(EnumType.STRING)
+	private Type type;
+}
+
+enum Type{
+	ADMIN, USER
 }
