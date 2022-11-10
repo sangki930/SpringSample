746ebbe8c3871a52aca40ac6c5175d338285f6bc
sangki930 <sangki931@gmail.com>
2022-11-10 21:56:01 +0900

    commit MyBatis Sample

--- SpringMyBatisSample/src/main/java/com/edu/SpringMyBatisSampleApplication.java
+++ SpringMyBatisSample/src/main/java/com/edu/SpringMyBatisSampleApplication.java
@@ -3,6 +3,7 @@ package com.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// MyBatis Sample
 @SpringBootApplication
 public class SpringMyBatisSampleApplication {
 
