1aec7c3db8f7fea2d0cb83267f42844d9199e246
sangki930 <sangki931@gmail.com>
2022-12-09 08:48:01 +0900

    Merge branch 'main' of https://github.com/sangki930/SpringSample.git into main

--- SpringDataJpaDemo/src/main/java/com/example/demo/service/sampletwo/SampleTwoService.java
+++ SpringDataJpaDemo/src/main/java/com/example/demo/service/sampletwo/SampleTwoService.java
@@ -2,7 +2,13 @@ package com.example.demo.service.sampletwo;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SampleTwoService {
 
+	private final SampleTwoService sampleTwoService;
+	
+	
 }
