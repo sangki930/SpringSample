9bf11b341062b6568a2e0bf0585b9a60be0b7346
sangki930 <sangki931@gmail.com>
2023-01-13 18:41:35 +0900

    commit MemberController

--- SpringBatchDemo/src/main/java/com/example/controller/MemberController.java
+++ SpringBatchDemo/src/main/java/com/example/controller/MemberController.java
@@ -1,7 +1,14 @@
 package com.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.domain.Member;
 import com.example.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -10,10 +17,22 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
 	private final MemberService memberService;
 	
-	// 추후 추가 예정 2023.01.10
+	@GetMapping
+	public ResponseEntity<Member> getMember(
+			@PathVariable Long id
+			){
+		Member member = memberService.getMember(id);
+		
+		return new ResponseEntity<Member>(HttpStatus.OK);
+	}
 	
+	@PatchMapping
+	public ResponseEntity<?> updateMember(){
+		return null;
+	}
 }
